30dd84d7f1a1086379e67970de8ed930bbded0ac
spez1998 <sjtmlde123@gmail.com>
2019-08-27 17:49:57 +0100

    Spawn and kill bars at ease

---
+++ .config/admiral.d/bspwm_workspaces.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if ! type bspc &> /dev/null; then
+	echo "bspc not found"
+	exit 1
+fi
+
+while read -r line; do 
+	echo -n "%{F#32302f}"
+	case $line in
+		W*)
+			IFS=':'
+			set -- ${line#?}
+			while [ $# -gt 0 ]; do
+				item="$1"
+				name="${item#?}"
+				case $item in
+					f*)
+						# free desktop
+						echo -n " %{A:bspc desktop -f $name:}"
+						echo -n "${name,,}"
+						echo -n "%{A} "
+						;;
+					o*)
+						# occupied desktop
+						echo -n " %{A:bspc desktop -f $name:}"
+						echo -n "$name"
+						echo -n "%{A} " 
+						;;
+					u*)
+						# urgent desktop
+						echo -n " %{A:bspc desktop -f $name:}"
+						echo -n "$name"
+						echo -n "%{A} "
+						;;
+				esac
+				shift
+			done
+	esac
+	echo -n -e "%{F#928374}%{B-}\ue0b0%{F-}"
+	echo
+done < <(bspc subscribe report)
---
+++ .config/lemonbar/python/main.py
@@ -0,0 +1,11 @@
+class Widget:
+    'Base class for all information groups in bar'
+    def __init__(self, pos, sep_l, sep_r, weight, fg_colour, bg_colour):
+        self.pos = pos
+        self.sep_l = sep_l
+        self.sep_r = sep_r
+        self.weight = weight
+        self.fg_colour = fg_colour
+        self.bg_colour = bg_colour
+
+hostname = 2
