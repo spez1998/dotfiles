bc444cf44ee2dfc575e74642f175483db4f4d709
spez1998 <sjtmlde123@gmail.com>
2019-08-30 16:04:00 +0100

    Trying out Rust

---
+++ bin/rust/guess/target/debug/deps/guess-264e9da0c9cf297d.d
@@ -0,0 +1,5 @@
+/home/sujit/bin/rust/guess/target/debug/deps/guess-264e9da0c9cf297d: src/main.rs
+
+/home/sujit/bin/rust/guess/target/debug/deps/guess-264e9da0c9cf297d.d: src/main.rs
+
+src/main.rs:
---
+++ bin/rust/guess/target/debug/guess.d
@@ -0,0 +1,1 @@
+/home/sujit/bin/rust/guess/target/debug/guess: /home/sujit/bin/rust/guess/src/main.rs
