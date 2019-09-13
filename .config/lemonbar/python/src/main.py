e6e338b82110485027036c0aff55723619c6f51c
spez1998 <sjtmlde123@gmail.com>
2019-09-13 14:48:00 +0100

    More testing for a wrapper

--- .config/lemonbar/python/src/main.py
+++ .config/lemonbar/python/src/main.py
@@ -1,9 +1,27 @@
 import os
 import sys
+import configparser
 
-path = "/home/sujit/.config/lemonbar/python/fifo/data"
-fifo = open(path, "r")
-for i in fifo:
-    print(i)
-fifo.close()
+class Bar:
+    def __init__(self,settings):
+        self.conf_admiral = settings[0]
+        self.width = settings[1]
+        self.height = settings[2]
+        
 
+def fifo():
+    path = "/home/sujit/.config/lemonbar/python/fifo/data"
+    fifo = open(path, "r")
+    for i in fifo:
+        print(i)
+    fifo.close()
+
+def ini_parser():
+    parser = configparser.ConfigParser()
+    parser.read('../conf/config.ini')
+    print(parser.sections())
+    sections = parser.sections()[0]
+    settings = [parser.get(sections,i) for i in parser[sections]]
+    bar_inst = Bar(settings)
+
+ini_parser()
